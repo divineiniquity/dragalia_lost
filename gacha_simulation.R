b6ce868d36bfabb25fc1581bc02553bea48bd399
divineiniquity <10665969+divineiniquity@users.noreply.github.com>
2020-09-26 10:26:34 +1000

    Fixed 101st pity roll
    Previously gave guaranteed feature unit, now gives guaranteed 5* unit. Can still get a feature unit on the 101st roll.

--- gacha_simulation.R
+++ gacha_simulation.R
@@ -2,14 +2,13 @@ dl_summon <- function(regular_rate = 0.04,
                       feature_rate = 0.005,
                       pity_rate = 0.005,
                       ten_pull_strat_threshold = 20,
-                      num_trials = 50000,
+                      num_trials = 10000,
                       seed_num = 123) {
   
   roll_counter <- integer(num_trials) # Initialize a vector of zeroes to store the number of rolls we take for each trial
   set.seed(seed_num) # Setting a seed to ensure reproducible results
   
   for (i in 1:num_trials) {
-    
     # Initialise the base rates for each attempt
     get_unit <- 0 # This flag changes to 1 once we get the feature unit (duplicates ignored)
     regular_rng <- regular_rate # This rate initially starts at the base rate, updates when the pity increases, and resets when a 5* is obtained
@@ -23,11 +22,11 @@ dl_summon <- function(regular_rate = 0.04,
         rng <- runif(1, min = 0, max = 1) # This is the RNG of the roll
         
         # If the feature unit is obtained, we are done
-        if(rng <= feature_rng | pity_counter >= 100) {
+        if(rng <= feature_rng) {
           get_unit <- 1
           
           # If a 5-star is obtained, reset the base rate, feature rate, and pity counter
-        } else if(rng <= regular_rng) {
+        } else if(rng <= regular_rng | pity_counter >= 100) {
           regular_rng <- regular_rate
           feature_rng <- feature_rate
           pity_counter <- 0
@@ -67,22 +66,22 @@ dl_summon <- function(regular_rate = 0.04,
   return(results)
 }
 
-dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 172.7346 | 60 | 240
-dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 188.8965 | 60 | 260
-dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 110.5135 | 40 | 150
-dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 120.757  | 40 | 170
+dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 174.4531 | 60 | 240
+dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 189.2233 | 60 | 260
+dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 111.248  | 40 | 150
+dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 0,num_trials = 50000,seed_num = 123) # 120.8914 | 40 | 170
 
-dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 169.7861 | 54 | 233
-dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 185.4344 | 58 | 256
-dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 108.3952 | 39 | 149
-dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 117.9589 | 39 | 163
+dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 171.5328 | 54 | 236
+dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 185.7616 | 58 | 256
+dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 109.1479 | 39 | 150
+dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 10,num_trials = 50000,seed_num = 123) # 118.062  | 39 | 163
 
-dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 168.5173 | 53 | 232
-dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 184.0471 | 56 | 254
-dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 106.8817 | 37 | 147
-dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 116.7169 | 37 | 161
+dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 170.2621 | 53 | 234
+dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 184.4073 | 56 | 254
+dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 107.6609 | 37 | 148
+dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 20,num_trials = 50000,seed_num = 123) # 116.8381 | 37 | 161
 
-dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 168.0055 | 52 | 231
-dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 183.811  | 55 | 254
-dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 106.2622 | 36 | 146
-dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 116.2098 | 36 | 161
+dl_summon(regular_rate = 0.04,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 169.7988 | 52 | 234
+dl_summon(regular_rate = 0.06,feature_rate = 0.005,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 184.163  | 55 | 255
+dl_summon(regular_rate = 0.04,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 107.0139 | 36 | 148
+dl_summon(regular_rate = 0.06,feature_rate = 0.008,pity_rate = 0.005,ten_pull_strat_threshold = 30,num_trials = 50000,seed_num = 123) # 116.3338 | 36 | 161
